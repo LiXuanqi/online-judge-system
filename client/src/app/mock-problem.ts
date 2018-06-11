da2f2f0e19236ff5c1261f2a1f095e3f174968c2
lixuanqi <lixuanqi1995@gmail.com>
2018-06-11 10:35:18 +0800

    import bootstrap and improve UI of problem-list

--- client/src/app/mock-problem.ts
+++ client/src/app/mock-problem.ts
@@ -5,10 +5,12 @@ export const PROBLEMS: Problem[] = [{
   id: 1,
   name: '2 sum',
   desc: 'the easy two sum',
+  acceptance: 0.394,
   difficulty: 'easy'
 }, {
   id: 2,
   name: '3 sum',
   desc: '3 sum a!!',
+  acceptance: 0.233,
   difficulty: 'medium'
 }];
--- client/src/app/models/problem.model.ts
+++ client/src/app/models/problem.model.ts
@@ -2,5 +2,6 @@ export class Problem {
   id: number;
   name: string;
   desc: string;
+  acceptance: number;
   difficulty: string;
 }
