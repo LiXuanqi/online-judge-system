58f9baf798bb398a58afa1ae5ccbb4192056adc7
lixuanqi <lixuanqi1995@gmail.com>
2018-06-16 16:46:17 +0800

    fast forwarding restore.

--- client/src/app/components/editor/editor.component.ts
+++ client/src/app/components/editor/editor.component.ts
@@ -71,8 +71,8 @@ export class EditorComponent implements OnInit {
       console.log('cursor moves: ' + JSON.stringify(cursor));
       this.collaboration.cursorMove(JSON.stringify(cursor));
     });
-
-    // this.collaboration.restoreBuffer();
+    
+    this.collaboration.restoreBuffer();
   }
 
   setLanguage(language: string): void {
--- server/services/socketService.js
+++ server/services/socketService.js
@@ -1,25 +1,50 @@
+let redisClient = require('../utils/redisClient');
+const TIMEOUT_IN_SECONDS = 3600;
+
 module.exports = function(io) {
   // store sessions
   let collaborations = [];
 
   let socketIdToSessionId = [];
 
+  let sessionPath = "/temp_sessions"
+
   io.on('connection', (socket) => {
     let sessionId = socket.handshake.query['sessionId'];
     socketIdToSessionId[socket.id] = sessionId;
-
-    if (!(sessionId in collaborations)) {
-      collaborations[sessionId] = {
-        'participants': []
-      };
-    }
-    // add a new person to a session.
-    collaborations[sessionId]['participants'].push(socket.id);
+    // new session.
+    if (sessionId in collaborations) {
+      // add a new person to a session.
+      collaborations[sessionId]['participants'].push(socket.id);
+    } else {
+      redisClient.get(sessionPath + '/' + sessionId, (data) => {
+        if (data) {
+          console.log("session terminiated previously; pulling back from Redis");
+          collaborations[sessionId] = {
+            'cachedChangeEvents': JSON.parse(data),
+            'participants': []
+          };  
+        } else {
+          console.log("creating new session")
+          collaborations[sessionId] = {
+            'cachedChangeEvents': [],
+            participants: []
+          }
+        }
+        collaborations[sessionId]['participants'].push(socket.id);
+      });
+    };
 
     socket.on('change', delta => {
       console.log("change from client: " + socketIdToSessionId[socket.id] + " " + delta);
+      let sessionId = socketIdToSessionId[socket.id];
+
+      if (sessionId in collaborations) {
+        collaborations[sessionId]['cachedChangeEvents'].push(["change", delta, Date.now()]);
+      }
+
       forwardEvents(socket.id, 'change', delta);
-    })
+    });
 
     socket.on('cursorMove', cursor => {
       console.log("cursorMove: " + socketIdToSessionId[socket.id] + " " + cursor);
@@ -27,7 +52,39 @@ module.exports = function(io) {
       cursor['socketId'] = socket.id;
 
       forwardEvents(socket.id, 'cursorMove', JSON.stringify(cursor));
-    })
+    });
+
+    socket.on('disconnect', () => {
+      let sessionId = socketIdToSessionId[socket.id];
+      console.log('socker ' + socket.id + 'disconnected.');
+
+      if (sessionId in collaborations) {
+        let participants = collaborations[sessionId]['participants'];
+        let index = participants.indexOf(socket.id);
+        if (index >= 0) {
+          participants.splice(index, 1);
+          if (participants.length == 0) {
+            console.log("last participant left. Storing in Redis.");
+            let key = sessionPath + "/" + sessionId;
+            let value = JSON.stringify(collaborations[sessionId]['cachedChangeEvents']);
+            redisClient.set(key, value, redisClient.redisPrint);
+            redisClient.expire(key, TIMEOUT_IN_SECONDS);
+            delete collaborations[sessionId];
+          }
+        }
+      }
+    });
+
+    socket.on('restoreBuffer', () => {
+      let sessionId = socketIdToSessionId[socket.id];
+      console.log("restoring buffer for session: " + sessionId + ', socket: ' + socket.id);
+      if (sessionId in collaborations) {
+        let changeEvents = collaborations[sessionId]['cachedChangeEvents'];
+        for (let i = 0; i < changeEvents.length; i++) {
+          socket.emit(changeEvents[i][0], changeEvents[i][1]);
+        }
+      }
+    });
 
     function forwardEvents(socketId, eventName, dataString) {
       let sessionId = socketIdToSessionId[socketId];
@@ -40,7 +97,7 @@ module.exports = function(io) {
           }
         }
       }
-    }
+    };
 
   })
 }
